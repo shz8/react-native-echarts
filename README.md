e61b88385f70ee3f95842226bb1bd892bb22da68
张红涛 <zhang.hongtao@zs-hospital.sh.cn>
2019-09-28 17:24:17 +0800

    修复一些问题

--- README.md
+++ README.md
@@ -4,9 +4,41 @@
   [![npm](https://img.shields.io/npm/dm/native-echarts.svg?style=flat)](https://www.npmjs.org/package/native-echarts)
   [![License](http://img.shields.io/npm/l/native-echarts.svg?style=flat)](https://raw.githubusercontent.com/somonus/react-native-echarts/master/LICENSE.md)
   
+
+## 修改记录
+1、native-echarts 在react-native-webview 替换webview后出错  
+  tmp.html改为动态：
+  `<!DOCTYPE html>
+    <html>
+      <head>
+        <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon"> 
+        <title>echarts</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <style type="text/css">
+          html,body {
+            height: 100%;
+            width: 100%;
+            margin: 0;
+            padding: 0;
+          }
+          #main {
+            height: 100%;
+          }
+        </style>
+        <script src="https://cdn.bootcss.com/echarts/4.3.0/echarts.min.js"></script>
+      </head>
+      
+      <body>
+        <div id="main"></div>
+      <body>
+    <html>`
+2、增加 tplurl 属性，可传入自定义的tmp.html
+
 ## install
 
-$ npm install native-echarts --save
+
+$ npm install native-echarts1 --save
 
 ## Usage
 
@@ -27,7 +59,7 @@ import {
   Text,
   View
 } from 'react-native';
-import Echarts from 'native-echarts';
+import Echarts from 'native-echarts1';
 
 export default class app extends Component {
   render() {
--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
+import { WebView } from 'react-native-webview';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 
@@ -9,10 +10,10 @@ export default class App extends Component {
     super(props);
     this.setNewOption = this.setNewOption.bind(this);
   }
-  
+
 
   componentWillReceiveProps(nextProps) {
-    if(nextProps.option !== this.props.option) {
+    if (nextProps.option !== this.props.option) {
       this.refs.chart.reload();
     }
   }
@@ -22,19 +23,47 @@ export default class App extends Component {
   }
 
   render() {
+    let { tplurl } = this.props
+    let html = `<!DOCTYPE html>
+    <html>
+      <head>
+        <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon"> 
+        <title>echarts</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <style type="text/css">
+          html,body {
+            height: 100%;
+            width: 100%;
+            margin: 0;
+            padding: 0;
+          }
+          #main {
+            height: 100%;
+          }
+        </style>
+        <script src="https://cdn.bootcss.com/echarts/4.3.0/echarts.min.js"></script>
+      </head>
+      
+      <body>
+        <div id="main"></div>
+      <body>
+    <html>
+    `
+    const source = tplurl?{ 'uri': tplurl }:{html}
     return (
-      <View style={{flex: 1, height: this.props.height || 400,}}>
+      <View style={{ flex: 1, height: this.props.height || 400, }}>
         <WebView
           ref="chart"
-          scrollEnabled = {false}
-          injectedJavaScript = {renderChart(this.props)}
+          scrollEnabled={false}
+          injectedJavaScript={renderChart(this.props)}
           style={{
             height: this.props.height || 400,
             backgroundColor: this.props.backgroundColor || 'transparent'
           }}
           scalesPageToFit={Platform.OS !== 'ios'}
           originWhitelist={['*']}
-          source={require('./tpl.html')}
+          source={source}
           onMessage={event => this.props.onPress ? this.props.onPress(JSON.parse(event.nativeEvent.data)) : null}
         />
       </View>
